c49294c133a86555da7c827496be393274ef59a2
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-11-23 22:18:09 -0600

    Done.

--- index.js
+++ index.js
@@ -3,13 +3,24 @@
 // function testFunc() {
 //   return "hi"
 // }
-const record = [
-  {year: "2018", result: "L"},
-  {year: "2017", result: "W"},
-  {year: "2016", result: "N/A"}
-]
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "W"},
+//   {year: "2016", result: "N/A"}
+// ]
 
 function superbowlWin(array) {
-  result = array.find()
+  console.log("array", array)
+  // array.find( function(item) {
+  //   console.log("item", item)
+  //   if (item === "W") {
+  //     return item
+  //   }
+  // })
 
+  let arrResult = array.find(item => item.result === "W")
+   if (arrResult !== undefined) {
+     return arrResult.year
+   }
+  
 }
